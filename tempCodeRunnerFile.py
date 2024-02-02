bd2cda0c3af284d29bb625d8e8b4b87a5f3b6adf
Ashish Pandey <pandaya10@gmail.com>
2024-02-02 22:42:18 +0530

    Create tempCodeRunnerFile.py

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# model.add(tf.keras.layers.Dense(128,activation='relu'))
+# model.add(tf.keras.layers.Dense(128, activation='relu'))
+# model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+# model.compile(optimizer='adam', loss= 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+
+# model.fit(x_train, y_train, epochs=3)
+
+# model.save('handwritten.model')
